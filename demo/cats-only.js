d04b9274561b1874ee0938cd33853614dd4dd49a
Elliott Marquez <emarquez@google.com>
2018-02-22 15:09:26 -0800

    auto-converted by polymer-modulizer

--- demo/cats-only.js
+++ demo/cats-only.js
@@ -1,4 +1,8 @@
-<!--
+import { html } from '../../polymer/polymer.js';
+import { IronValidatableBehavior } from '../../iron-validatable-behavior/iron-validatable-behavior.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,12 +10,9 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-<link rel="import" href="../../polymer/polymer.html">
-<link rel="import" href="../../iron-validatable-behavior/iron-validatable-behavior.html">
-
-<dom-module id="cats-only">
-  <template>
+*/
+Polymer({
+  _template: html`
     <style>
       :host {
         display: inline-block;
@@ -24,42 +25,35 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
       }
     </style>
     <input value="{{value}}" id="input" placeholder="Type 'cats' to prove you like cats">
-  </template>
-</dom-module>
+`,
 
-<script>
+  is: 'cats-only',
 
-  Polymer({
-    is: 'cats-only',
-
-    properties: {
-      value: {
-        type: String
-      },
-
-      name: {
-        type: String
-      }
+  properties: {
+    value: {
+      type: String
     },
 
-    behaviors: [
-      Polymer.IronValidatableBehavior
-    ],
-
-    listeners: {
-      'input': '_onInput'
-    },
-
-    _onInput: function() {
-      this.value = this.$.input.value;
-    },
-
-    // Overidden from Polymer.IronValidatableBehavior. Will set the `invalid`
-    // attribute automatically, which should be used for styling.
-    _getValidity: function() {
-      return this.value === 'cats';
+    name: {
+      type: String
     }
+  },
 
-  });
+  behaviors: [
+    IronValidatableBehavior
+  ],
 
-</script>
+  listeners: {
+    'input': '_onInput'
+  },
+
+  _onInput: function() {
+    this.value = this.$.input.value;
+  },
+
+  // Overidden from Polymer.IronValidatableBehavior. Will set the `invalid`
+  // attribute automatically, which should be used for styling.
+  _getValidity: function() {
+    return this.value === 'cats';
+  }
+});
